a8e6208917e1ef4dbbdb90158d45cd18b6157348
roket64 <1p41i4y1@gmail.com>
2022-01-10 22:19:27 +0900

    property fix

--- src/propertyTest/PropertyMain.java
+++ src/propertyTest/PropertyMain.java
@@ -3,21 +3,13 @@ package propertyTest;
 public class PropertyMain {
     public static void main(String[] args) {
         String testKeyName = "testKeyName";
-        String testKeyName02 = "testKeyName02";
         String testValue = "testValue";
-        String testValue02 = "testValue02";
         PropertyTest test = new PropertyTest(testKeyName, testValue);
-        PropertyTest test02 = new PropertyTest(testKeyName02, testValue02);
         String returnValue;
-        String returnValue02;
 
         test.setFilePath();
         returnValue = test.getProperty();
 
-        test02.setFilePath();
-        returnValue02 = test02.getProperty();
-
         System.out.println(returnValue);
-        System.out.println(returnValue02);
     }
 }
--- src/propertyTest/PropertyTest.java
+++ src/propertyTest/PropertyTest.java
@@ -1,14 +1,12 @@
 package propertyTest;
 
 import java.io.*;
-import java.util.HashMap;
 import java.util.Properties;
 
 public class PropertyTest {
     private String keyName;
     private String keyValue;
     private File configFile;
-    //프로퍼티 파일의 경로를 미리 정해주었으나 필요에 따라 바뀔 수도 있음
     private static final String configFilePath = "src/propertyTest/config.properties";
 
     public PropertyTest(String keyName, String keyValue){
@@ -18,15 +16,11 @@ public class PropertyTest {
     }
 
     public void setFilePath(){
-        //프로퍼티 파일이 존재하는지, 존재한다면 파일인지 여부 확인.
         if (configFile.exists() || configFile.isFile()){
-            //조건에 부합하면 프로퍼티 값 설정.
             setProperty();
         }else{
             try {
-                //부합하지 않으면 새로운 파일 생성.
                 FileOutputStream fos = new FileOutputStream(configFile, true);
-                //파일 생성 후 프로퍼티 값 설정.
                 setProperty();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -38,16 +32,8 @@ public class PropertyTest {
         try {
             FileWriter fw = new FileWriter(configFile);
             BufferedWriter bw = new BufferedWriter(fw);
-            /* 매개변수로 받아온 문자열을 조합해 저장한다.
-            hashMap이나 배열을 이용할 수도 있을 것 같다.*/
             String twinValue = keyName + "=" + keyValue + "\n";
-            /*
-            //TODO
-            새로운 key값이 입력되면 기존의 key에 덮어쓰는 문제가 있다.
-            고치려면 어떻게 해야할까
-             */
-            HashMap<String, String> twins = new HashMap<>();
-            twins.put(keyName, keyValue);
+
             bw.write(twinValue);
             bw.flush();
             bw.close();
