e233a6139b7b9f93a6359cc51cbbdbe9e34c3079
roket64 <1p41i4y1@gmail.com>
2022-01-10 18:03:23 +0900

    Generic fix

--- src/genericExamples/GenericExam02.java
+++ src/genericExamples/GenericExam02.java
@@ -4,14 +4,14 @@ package genericExamples;
 Generic은 꺽쇠<>를 써서 사용할 수 있다.
 이때 e는 가상의 클래스이며, 이름은 임의로 바꿀 수 있다.
  */
-public class GenericExam02<e>{
-    private e object;
+public class GenericExam02<E>{
+    private E object;
 
-    public void setObject(e object){
+    public void setObject(E object){
         this.object = object;
     }
 
-    public e getObject(){
+    public E getObject(){
         return object;
     }
 }
