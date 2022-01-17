3b1e8e3e56ccec9684ac50a98b303d4588322fc6
roket64 <1p41i4y1@gmail.com>
2022-01-17 20:42:32 +0900

    after fix

--- src/me/roket/tests/testingground/Main.java
+++ src/me/roket/tests/testingground/Main.java
@@ -1,15 +1,8 @@
 package me.roket.tests.testingground;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        byte[] a = br.readLine().getBytes(StandardCharsets.US_ASCII);
-        bw.write(String.valueOf(a[0]));
-        br.close();
-        bw.close();
     }
 }
